7484cd417448f99426042455b499bd9016b4cdb8
Anzas <Anzas@ACE>
2024-08-29 00:52:51 +0200

    Add project files.

---
+++ Sales_system/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Sales_system
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bagType = new System.Windows.Forms.ListBox();
+            this.Numericquantity = new System.Windows.Forms.NumericUpDown();
+            this.Overnightbtn = new System.Windows.Forms.RadioButton();
+            this.Three_Daybtn = new System.Windows.Forms.RadioButton();
+            this.Standardbtn = new System.Windows.Forms.RadioButton();
+            this.CalculateButton_Click = new System.Windows.Forms.Button();
+            this.ClearButton_Click = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Numericquantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bagType
+            // 
+            this.bagType.FormattingEnabled = true;
+            this.bagType.ItemHeight = 16;
+            this.bagType.Items.AddRange(new object[] {
+            "Full Decorative ",
+            "Beaded",
+            "Needlepoint Design",
+            "Fringed Beaded",
+            "Plain"});
+            this.bagType.Location = new System.Drawing.Point(24, 113);
+            this.bagType.Name = "bagType";
+            this.bagType.Size = new System.Drawing.Size(164, 132);
+            this.bagType.TabIndex = 0;
+            // 
+            // Numericquantity
+            // 
+            this.Numericquantity.Location = new System.Drawing.Point(630, 88);
+            this.Numericquantity.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Numericquantity.Name = "Numericquantity";
+            this.Numericquantity.Size = new System.Drawing.Size(120, 22);
+            this.Numericquantity.TabIndex = 1;
+            this.Numericquantity.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Overnightbtn
+            // 
+            this.Overnightbtn.AutoSize = true;
+            this.Overnightbtn.Location = new System.Drawing.Point(94, 265);
+            this.Overnightbtn.Name = "Overnightbtn";
+            this.Overnightbtn.Size = new System.Drawing.Size(85, 20);
+            this.Overnightbtn.TabIndex = 2;
+            this.Overnightbtn.TabStop = true;
+            this.Overnightbtn.Text = "Overnight";
+            this.Overnightbtn.UseVisualStyleBackColor = true;
+            // 
+            // Three_Daybtn
+            // 
+            this.Three_Daybtn.AutoSize = true;
+            this.Three_Daybtn.Location = new System.Drawing.Point(344, 264);
+            this.Three_Daybtn.Name = "Three_Daybtn";
+            this.Three_Daybtn.Size = new System.Drawing.Size(92, 20);
+            this.Three_Daybtn.TabIndex = 3;
+            this.Three_Daybtn.TabStop = true;
+            this.Three_Daybtn.Text = "Three Day";
+            this.Three_Daybtn.UseVisualStyleBackColor = true;
+            // 
+            // Standardbtn
+            // 
+            this.Standardbtn.AutoSize = true;
+            this.Standardbtn.Location = new System.Drawing.Point(560, 265);
+            this.Standardbtn.Name = "Standardbtn";
+            this.Standardbtn.Size = new System.Drawing.Size(83, 20);
+            this.Standardbtn.TabIndex = 4;
+            this.Standardbtn.TabStop = true;
+            this.Standardbtn.Text = "Standard";
+            this.Standardbtn.UseVisualStyleBackColor = true;
+            // 
+            // CalculateButton_Click
+            // 
+            this.CalculateButton_Click.Location = new System.Drawing.Point(323, 363);
+            this.CalculateButton_Click.Name = "CalculateButton_Click";
+            this.CalculateButton_Click.Size = new System.Drawing.Size(160, 43);
+            this.CalculateButton_Click.TabIndex = 5;
+            this.CalculateButton_Click.Text = "Calculate Total";
+            this.CalculateButton_Click.UseVisualStyleBackColor = true;
+            this.CalculateButton_Click.Click += new System.EventHandler(this.CalculateButton_Click_Click);
+            // 
+            // ClearButton_Click
+            // 
+            this.ClearButton_Click.Location = new System.Drawing.Point(516, 363);
+            this.ClearButton_Click.Name = "ClearButton_Click";
+            this.ClearButton_Click.Size = new System.Drawing.Size(164, 43);
+            this.ClearButton_Click.TabIndex = 6;
+            this.ClearButton_Click.Text = "Clear Selection";
+            this.ClearButton_Click.UseVisualStyleBackColor = true;
+            this.ClearButton_Click.Click += new System.EventHandler(this.ClearButton_Click_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ClearButton_Click);
+            this.Controls.Add(this.CalculateButton_Click);
+            this.Controls.Add(this.Standardbtn);
+            this.Controls.Add(this.Three_Daybtn);
+            this.Controls.Add(this.Overnightbtn);
+            this.Controls.Add(this.Numericquantity);
+            this.Controls.Add(this.bagType);
+            this.Name = "Form1";
+            this.Text = "AD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Numericquantity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox bagType;
+        private System.Windows.Forms.NumericUpDown Numericquantity;
+        private System.Windows.Forms.RadioButton Overnightbtn;
+        private System.Windows.Forms.RadioButton Three_Daybtn;
+        private System.Windows.Forms.RadioButton Standardbtn;
+        private System.Windows.Forms.Button CalculateButton_Click;
+        private System.Windows.Forms.Button ClearButton_Click;
+    }
+}
+
---
+++ Sales_system/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sales_system
+{
+    public partial class Form1 : Form
+    {
+        private ListBox bagTypeListBox;
+        private NumericUpDown quantityNumericUpDown;
+        private RadioButton overnightRadioButton;
+        private RadioButton threeDayRadioButton;
+        private RadioButton standardRadioButton;
+        private Button calculateButton;
+        private Button clearButton;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+        private void InitializeComponents()
+        {
+            bagTypeListBox = new ListBox
+            {
+                Name = "bagTypeListBox",
+                Location = new Point(10, 10),
+                Size = new Size(200, 100)
+
+            };
+            bagTypeListBox.Items.AddRange(new string[] { "Full Decorative", "Beaded", "Needlepoint Design", "Fringed Beaded", "Plain" });
+            this.Controls.Add(bagTypeListBox);
+
+            quantityNumericUpDown = new NumericUpDown
+            {
+                Name = "quantityNumericUpDown",
+                Location = new Point(10, 120),
+                Size = new Size(100, 20),
+                Minimum = 1,
+                Value = 1
+            };
+            this.Controls.Add(quantityNumericUpDown);
+            overnightRadioButton = new RadioButton
+            {
+                Name = "overnightRadioButton",
+                Text = "Overnight",
+                Location = new Point(10, 150)
+            };
+            threeDayRadioButton = new RadioButton
+            {
+                Name = "threeDayRadioButton",
+                Text = "Three Day",
+                Location = new Point(10, 180)
+            };
+            Standardbtn = new RadioButton
+            {
+                Name = "standardRadioButton",
+                Text = "Standard",
+                Location = new Point(10, 210)
+            };
+            this.Controls.Add(Overnightbtn);
+            this.Controls.Add(Three_Daybtn);
+            this.Controls.Add(Standardbtn);
+
+            calculateButton = new Button
+            {
+                Name = "clearButton",
+                Text = "Clear Selection",
+                Location = new Point(120, 240),
+                Size = new Size(100, 30)
+            };
+            clearButton = new Button
+            {
+                Name = "clearButton",
+                Text = "Clear Selection",
+                Location = new Point(120, 240),
+                Size = new Size(100, 30)
+            };
+            clearButton.Click += new EventHandler(ClearButton_Click_Click);
+            this.Controls.Add(clearButton);
+
+            this.Text = "Handbag Sales System";
+            this.Size = new Size(250, 320);
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalculateButton_Click_Click(object sender, EventArgs e)
+        {
+            string selectedBag = bagType.SelectedItem?.ToString();
+            if (selectedBag == null)
+            {
+                MessageBox.Show("Please select a bag type.");
+                return;
+            }
+            int quantity = (int)Numericquantity.Value;
+
+            // Determine price based on bag type
+            int price = 0;
+            if (selectedBag == "Full Decorative")
+            {
+                price = 450;
+            }
+            else if (selectedBag == "Beaded")
+            {
+                price = 560;
+            }
+            else if (selectedBag == "Needlepoint Design")
+            {
+                price = 240;
+            }
+            else if (selectedBag == "Fringed Beaded")
+            {
+                price = 630;
+            }
+            else if (selectedBag == "Plain")
+            {
+                price = 120;
+            }
+            double shippingRate = 0;
+            if (Overnightbtn.Checked) shippingRate = 0.10;
+            else if (Three_Daybtn.Checked) shippingRate = 0.07;
+            else if (Standardbtn.Checked) shippingRate = 0.05;
+            else
+            {
+                MessageBox.Show("Please select a shipping option.");
+                return;
+            }
+            double totalCost = quantity * price * (1 + shippingRate);
+
+            MessageBox.Show($"Selection: {selectedBag}\nQuantity: {quantity}\nShipping: {shippingRate * 100}%\nTotal Cost: R{totalCost:F2}");
+
+        }
+
+        private void ClearButton_Click_Click(object sender, EventArgs e)
+        {
+            bagType.ClearSelected();
+            Numericquantity.Value = 1;
+            Overnightbtn.Checked = false;
+            Three_Daybtn.Checked = false;
+            Standardbtn.Checked = false;
+            MessageBox.Show("Selections cleared.");
+        }
+        
+
+
+    }
+}
